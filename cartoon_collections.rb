baec6009810450c61788b14a88ff256fe30f3956
Deborah Santillan <s.debb@yahoo.com>
2020-06-19 02:25:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,15 @@
-def roll_call_dwarves
-  roll_call_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  
+def roll_call_dwarves(array)
+  array.each_with_index do |n, index| 
+   puts "#{index + 1} #{n}"   
+  end  
+end
+def summon_captain_planet(planteer_calls)
+  planteer_calls.map! {|calls| calls.capitalize + "!"}
+end
+def long_planeteer_calls(calls)
+  calls.any? {|n| n.size > 4}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
