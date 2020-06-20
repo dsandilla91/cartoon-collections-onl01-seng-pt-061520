28e8350e5cf7ee0309c099f40db67ab676c8628f
Deborah Santillan <s.debb@yahoo.com>
2020-06-20 01:25:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese(cheese_types)
-  cheese_types.include?
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |a| cheese_types.include?(a) }
 end
