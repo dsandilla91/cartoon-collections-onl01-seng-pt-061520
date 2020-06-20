69a831443f98eb0e6a281ba777e519e5432f9b40
Deborah Santillan <s.debb@yahoo.com>
2020-06-20 00:53:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  cheese_types.include 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
